58af158db0c083c50c1d888d1e81f1b5ddec3fa0
NathanGloyn <nathans.dropbox@gmail.com>
2016-06-12 20:58:54 +0100

    Alter rollup to generate source maps & uglify the code

--- Babel_rollup/gulpfile.js
+++ Babel_rollup/gulpfile.js
@@ -1,14 +1,35 @@
 var gulp = require('gulp'),
+    clean = require('gulp-clean'),
     rollup = require('rollup-stream'),
     babel = require('gulp-babel'),
     source = require('vinyl-source-stream'),
-    buffer = require('vinyl-buffer');
+    buffer = require('vinyl-buffer'),
+    sourcemaps = require('gulp-sourcemaps')
+    uglify = require('gulp-uglify'),
+    serve = require('gulp-serve');
 
+gulp.task('clean', function(){
+    return gulp.src('build/')
+               .pipe(clean());
+});
 
-   gulp.task('default', function() {
-        return rollup({ entry: 'app/js/entry.js'})
-               .pipe(source('app.js'))
-               .pipe(buffer())
-               .pipe(babel())
-               .pipe(gulp.dest('build')); 
-   });
+gulp.task('build', function(){
+    return gulp.src(['app/*.html'])
+           .pipe(gulp.dest('build'));
+});
+
+gulp.task('serve',['default'], serve(['build']));
+
+gulp.task('default', ['clean', 'build'] ,function() {
+    return rollup({ 
+                entry: 'app/js/entry.js',
+                sourceMap: true
+            })
+            .pipe(source('app.js'))
+            .pipe(buffer())
+            .pipe(sourcemaps.init({loadMaps: true}))
+            .pipe(babel())
+            .pipe(uglify())   
+            .pipe(sourcemaps.write('.')) 
+            .pipe(gulp.dest('build/')); 
+});
