1c7d72b3e8eb86fc5e8364e0d1ac702d1983bd96
NathanGloyn <nathans.dropbox@gmail.com>
2016-06-17 20:18:44 +0100

    Alter webpack to gemnerate source maps & uglify code

--- Bable_webpack/webpack.config.js
+++ Bable_webpack/webpack.config.js
@@ -1,13 +1,25 @@
+var webpack = require("webpack");
+
 module.exports = {
 	entry: './app/js/entry.js',
 	output: {
-		filename: 'bundle.js'
+		filename: 'bundle.js',
+		soureMapFilename: 'bundle.map'
 	},
+	devtool: 'sourcemap',
 	module: {
 		loaders: [{
 			test: /\.js$/,
 			exclude: /node_modules/,
 			loader: 'babel-loader'
-		}]	
-	}
+		}]
+	},
+	plugins: [
+		new webpack.optimize.UglifyJsPlugin({
+    		sourceMap: true,
+			compress: {
+				warnings: false
+			}	
+		})
+	]
 }
