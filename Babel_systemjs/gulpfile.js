ef76d446c4dd0a487cce8afed8bc4fbf52eb936b
NathanGloyn <nathans.dropbox@gmail.com>
2016-06-26 19:39:14 +0100

    System JS minification & source map creation added to build

--- Babel_systemjs/gulpfile.js
+++ Babel_systemjs/gulpfile.js
@@ -1,14 +1,16 @@
 var gulp = require('gulp');
 var print = require('gulp-print');
 var babel = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
 var webserver = require('gulp-webserver');
 
 gulp.task('js', function () {
 	return gulp.src('app/**/*.js')
-			   .pipe(print())
-        .pipe(babel({
-            presets: ['es2015']
-        }))
+			   .pipe(sourcemaps.init({loadMaps:true})) 
+               .pipe(babel())
+			   .pipe(uglify())
+			   .pipe(sourcemaps.write('.'))
 			   .pipe(gulp.dest('build'));
 });
 
@@ -16,13 +18,11 @@ gulp.task('libs', function() {
 	return gulp.src([
 				'node_modules/systemjs/dist/system.js',
 				'node_modules/babel-polyfill/dist/polyfill.js'])
-			   .pipe(print())
 			   .pipe(gulp.dest('build/libs'));
 });
 
 gulp.task('build',['js','libs'], function() {
 	return gulp.src(['app/**/*.html', 'app/**/*.css'])
-			   .pipe(print())
 			   .pipe(gulp.dest('build'));
 });
 
