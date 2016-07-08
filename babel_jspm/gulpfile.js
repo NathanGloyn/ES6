e22ac88ef8472a76cb929ebf78525fca14f06c7b
NathanGloyn <nathans.dropbox@gmail.com>
2016-07-08 19:47:42 +0100

    Reorganize folders

--- babel_jspm/gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var gulp_jspm = require('gulp-jspm');
-
-gulp.task('default', function(){
-   return gulp.src('src/main.js')
-            .pipe(gulp_jspm())
-            .pipe(gulp.dest('build/')); 
-});
